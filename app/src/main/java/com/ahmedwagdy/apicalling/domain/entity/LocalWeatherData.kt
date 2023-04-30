7fa2ff80bf5d787fe4d5fd510418c4b99fc51171
Ahmed Wagdy Rashad <a7med.wagdy96@gmail.com>
2023-04-30 17:22:55 +0300

    add local weather data class

---
+++ app/src/main/java/com/ahmedwagdy/apicalling/domain/entity/LocalWeatherData.kt
@@ -0,0 +1,11 @@
+package com.ahmedwagdy.apicalling.domain.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "weather_data")
+data class LocalWeatherData(
+    @PrimaryKey(false)
+    val id: Int,
+   val weatherDataList: List<WeatherData>
+)
--- app/src/main/java/com/ahmedwagdy/apicalling/domain/entity/WeatherData.kt
+++ app/src/main/java/com/ahmedwagdy/apicalling/domain/entity/WeatherData.kt
@@ -3,9 +3,7 @@ package com.ahmedwagdy.apicalling.domain.entity
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "weather_data")
 data class WeatherData(
-    @PrimaryKey(false)
     val dt_txt: String,
     val temp: Double,
     val humidity: Int,
