b6a9a36bef60df2568556002bf0515bf4434b9d3
Ahmed Wagdy Rashad <a7med.wagdy96@gmail.com>
2023-04-30 17:38:40 +0300

    fix databas issue

--- app/src/main/java/com/ahmedwagdy/apicalling/domain/entity/LocalWeatherData.kt
+++ app/src/main/java/com/ahmedwagdy/apicalling/domain/entity/LocalWeatherData.kt
@@ -3,9 +3,11 @@ package com.ahmedwagdy.apicalling.domain.entity
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "weather_data")
+//@Entity(tableName = "weather_data")
 data class LocalWeatherData(
-    @PrimaryKey(false)
+  //  @PrimaryKey(false)
     val id: Int,
    val weatherDataList: List<WeatherData>
 )
+
+// add type converter to store list in database
--- app/src/main/java/com/ahmedwagdy/apicalling/domain/entity/WeatherData.kt
+++ app/src/main/java/com/ahmedwagdy/apicalling/domain/entity/WeatherData.kt
@@ -3,7 +3,9 @@ package com.ahmedwagdy.apicalling.domain.entity
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
+@Entity("weather_data")
 data class WeatherData(
+    @PrimaryKey
     val dt_txt: String,
     val temp: Double,
     val humidity: Int,
